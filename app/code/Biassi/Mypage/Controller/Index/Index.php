6300cf6ee08218c8052fc86b692d9ec338a14efb
Rodrigo Biassi <rodrigobiassi.net@gmail.com>
2017-09-17 13:53:45 -0300

    My First Module - Magento 2 (test)

---
+++ app/code/Biassi/Mypage/Controller/Index/Index.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Biassi\Mypage\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+class Index extends Action
+{
+    /** @var PageFactory  */
+    protected $resultPageFactory;
+
+    /**
+     * Index constructor.
+     * @param Context $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+    /**
+     * Load the page defined in view/frontend/layout/mypage_index_index.xml
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        return $this->resultPageFactory->create();
+    }
+}
---
+++ app/code/Biassi/Mypage/registration.php
@@ -0,0 +1,7 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Biassi_Mypage',
+    __DIR__
+);
